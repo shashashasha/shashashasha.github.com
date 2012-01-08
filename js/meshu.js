44c21d3071e0ba1d178a3b0fcfecb86163983a0b
binx <rachelbinx@gmail.com>
2012-01-08 11:54:42 -0800

    added geocoding to the input field

--- js/meshu.js
+++ js/meshu.js
@@ -1,4 +1,5 @@
 var sb = sb || {};
+var app_key = "dj0yJmk9M1hsekZBSDY1ZjRxJmQ9WVdrOU5uUjZiRzE0TXpRbWNHbzlNVEV5TURZMU1qRTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD00OQ--";
 
 sb.meshu = function(frame) {
 	var self = {},
@@ -14,13 +15,24 @@ sb.meshu = function(frame) {
 	// this is tied to a global submit button for now
     $("#submit").click(function(){
         var input = $("#coords").val();
+        var query = input.replace(" ","+");
+
+        $.ajax({
+            url: "http://where.yahooapis.com/geocode?location="+query+"&flags=J&appid="+app_key,
+            cache: false,
+            success: function(data){
+                var place = data.ResultSet.Results[0];
+                //console.log(place.longitude, place.latitude);
+                mesh.add(place.latitude, place.longitude);
+                map.updateBounds(mesh.lats(), mesh.lons());
+            }
+        });
 
         // random coordinate
-        var coords = [(Math.random() * 8) + 33, -120 + (Math.random() * 10)];
+        //var coords = [(Math.random() * 140) - 70, -180 + (Math.random() * 360)];
         // input.split(",");
 
-        mesh.add(coords[0], coords[1]);
-        map.updateBounds(mesh.lats(), mesh.lons());
+        //mesh.add(coords[0], coords[1]);
     });
 
 	// this is tied to a global output button for now
