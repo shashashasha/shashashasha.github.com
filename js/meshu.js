4f21c1adbbc076f9707c660c812f12e80f755a0d
binx <rachelbinx@gmail.com>
2012-01-08 12:20:50 -0800

    added support for unclear searches

--- js/meshu.js
+++ js/meshu.js
@@ -21,19 +21,36 @@ sb.meshu = function(frame) {
             url: "http://where.yahooapis.com/geocode?location="+query+"&flags=J&appid="+app_key,
             cache: false,
             success: function(data){
-                var place = data.ResultSet.Results[0];
-                //console.log(place.longitude, place.latitude);
-                mesh.add(place.latitude, place.longitude);
-                map.updateBounds(mesh.lats(), mesh.lons());
+                var results = data.ResultSet.Results;
+                $("#cases").empty();
+
+                if (results.length == 1)
+                    addPoint(results[0].latitude,results[0].longitude);
+                else {
+                    for (var i = 0; i < results.length; i++) {
+                        var r = results[i];
+                        $("<p>").text(r.city+", "+r.state+", "+r.country)
+                            .addClass("maybe-place")
+                            .data({"latitude":r.latitude,"longitude":r.longitude})
+                            .appendTo("#cases");
+                    }
+                    $("#cases p").click(function(){
+                        var r = $(this);
+                        addPoint(r.data("latitude"),r.data("longitude"));
+                        $("#cases").empty();
+                    });
+                }
             }
         });
 
         // random coordinate
         //var coords = [(Math.random() * 140) - 70, -180 + (Math.random() * 360)];
-        // input.split(",");
-
-        //mesh.add(coords[0], coords[1]);
     });
+
+    function addPoint(lat,lon) {
+        mesh.add(lat,lon);
+        map.updateBounds(mesh.lats(), mesh.lons());
+    }
 
 	// this is tied to a global output button for now
     $("#output").click(function(){
